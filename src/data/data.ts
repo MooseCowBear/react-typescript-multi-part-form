78812e89eaa7a8b2c2074231995986044747977d
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-01-07 15:38:08 -0500

    add data

---
+++ src/data/data.ts
@@ -0,0 +1,38 @@
+export const plans = [
+  {
+    title: "Arcade",
+    monthly: 9,
+    yearly: 90,
+    promo: "2 months free"
+  },
+  {
+    title: "Advanced",
+    monthly: 12,
+    yearly: 120,
+    promo: "2 months free"
+  },
+  {
+    title: "Pro",
+    monthly: 15,
+    yearly: 150,
+    promo: "2 months free"
+  }
+]
+
+export const addOns = [
+  {
+    title: "Online service",
+    monthly: 1,
+    yearly: 10
+  },
+  {
+    title: "Larger storage",
+    monthly: 2,
+    yearly: 20
+  },
+  {
+    title: "Customizable profile",
+    monthly: 2,
+    yearly: 20
+  }
+]
