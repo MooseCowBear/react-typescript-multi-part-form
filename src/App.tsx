8426b6e3f739d06b95f72b56b165dc99c0a76606
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-01-08 09:34:43 -0500

    position buttons on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import { useState } from "react";
 import { Sidebar } from "./layout/Sidebar";
 import { Form } from "./layout/Form";
+import { buttons } from "./helpers/buttons";
+import { FormButton } from "./components/FormButton";
 
 function App() {
-  const [currentStep, setCurrentStep] = useState(1);
+  const [currentStep, setCurrentStep] = useState(2);
 
   return (
     <>
       <Sidebar currentStep={currentStep} setStep={setCurrentStep} />
-      <div className="bg-neutral-300 md:bg-neutral-100 relative h-full p-5 md:p-0">
+      <div className="bg-neutral-300 md:bg-neutral-100 relative h-full p-5 md:p-0 relative">
         <Form step={currentStep} />
+        <div className="absolute p-3 bg-neutral-100 bottom-0 left-0 block w-full md:hidden">
+          {buttons(currentStep).map((button) => (
+            <FormButton name={button} />
+          ))}
+        </div>
       </div>
-      
     </>
   );
 }
