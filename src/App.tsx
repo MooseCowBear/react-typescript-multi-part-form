19d151822cf2f98173af5731af105f42c53ab446
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-01-08 08:03:29 -0500

    add classes for mobile layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,14 @@ import { Form } from "./layout/Form";
 function App() {
   const [currentStep, setCurrentStep] = useState(1);
 
+  //
+
   return (
     <>
-      <Sidebar currentStep={currentStep} setStep ={setCurrentStep}/>
-      <Form />
+      <Sidebar currentStep={currentStep} setStep={setCurrentStep} />
+      <div className="bg-blue-100 md:bg-neutral-100 relative h-full border p-5 md:p-0">
+        <Form step={currentStep} />
+      </div>
     </>
   );
 }
