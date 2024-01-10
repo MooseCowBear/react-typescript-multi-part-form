d5d3a22cb60c853df2b8e2f89102559bfeb32e7e
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-01-10 13:01:49 -0500

    update input to use context, add type as prop

--- src/components/FormFieldsOne.tsx
+++ src/components/FormFieldsOne.tsx
@@ -1,12 +1,11 @@
-import { TextInput } from "./TextInput";
+import { FormInput } from "./FormInput";
 
 export function FormFieldsOne() {
-  //todo: add labels
   return (
     <form className="flex flex-col gap-5">
-      <TextInput name="name" label="Name" placeholder="e.g. Stephen King"/>
-      <TextInput name="email" label="Email" placeholder="e.g. stephenking@lorem.com"/>
-      <TextInput name="phone" label="Phone Number" placeholder="e.g. +1 234 567 890"/>
+      <FormInput name="name" label="Name" placeholder="e.g. Stephen King" />
+      <FormInput name="email" label="Email" placeholder="e.g. stephenking@lorem.com" type="email"/>
+      <FormInput name="phone" label="Phone Number" placeholder="e.g. +1 234 567 890"/>
     </form>
   );
 }
---
+++ src/components/FormInput.tsx
@@ -0,0 +1,41 @@
+import { useFormContext } from "../contexts/FormContext";
+
+type FormInputProps = {
+  name: string;
+  label: string;
+  placeholder: string;
+  type?: string;
+};
+
+export function FormInput({
+  name,
+  label,
+  placeholder,
+  type = "text",
+}: FormInputProps) {
+  const { personalInfo, setPersonalInfo } = useFormContext();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPersonalInfo((val) => {
+      const data = { ...val };
+      data[name] = e.target.value;
+      return data;
+    });
+  };
+
+  return (
+    <div className="flex flex-col gap-1">
+      <label htmlFor={name} className="text-sm">
+        {label}
+      </label>
+      <input
+        onChange={handleChange}
+        className="border border-neutral-400 rounded-md py-2 px-3 focus:outline-none focus:border-blue-400"
+        type={type}
+        id={name}
+        placeholder={placeholder}
+        value={personalInfo[name]}
+      />
+    </div>
+  );
+}
