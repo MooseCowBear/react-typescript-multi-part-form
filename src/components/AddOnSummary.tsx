f99591c74156a33813d7cde37201623c8d6ca65f
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-01-11 06:31:21 -0500

    add add on summary component, update form fields for fourth step

---
+++ src/components/AddOnSummary.tsx
@@ -0,0 +1,26 @@
+import { useFormContext } from "../contexts/FormContext";
+
+interface AddOn {
+  // TODO: move all the shared interfaces to file?
+  title: string;
+  subtitle: string;
+  monthly: number;
+  yearly: number;
+}
+
+type AddOnSummaryProps = {
+  addOn: AddOn;
+};
+
+export function AddOnSummary({ addOn }: AddOnSummaryProps) {
+  const {monthly} = useFormContext();
+
+  return (
+    <div className="flex justify-between items-center">
+      <h2 className="text-netural-500 text-sm">{addOn.title}</h2>
+      <span className="text-sm">{`+$${monthly ? addOn.monthly : addOn.yearly}/${
+        monthly ? "mo" : "yr"
+      }`}</span>
+    </div>
+  );
+}
--- src/components/FormFieldsFour.tsx
+++ src/components/FormFieldsFour.tsx
@@ -1,20 +1,46 @@
+import { useFormContext } from "../contexts/FormContext";
+import { total } from "../helpers/calculations";
+import { AddOnSummary } from "./AddOnSummary";
+
 export function FormFieldsFour() {
+  const { selectedPlan, addOns, monthly } = useFormContext();
+
   return (
     <div className="flex flex-col items-stretch">
       <div className="bg-blue-300/5 p-5 rounded-xl flex flex-col gap-6">
         <div className="flex justify-between items-center">
           <div className="flex flex-col items-start">
-            <h2 className="font-bold">something</h2>
-            <button className="text-sm underline text-neutral-500">Change</button>
+            <h2 className="font-medium">{`${selectedPlan.title} (${
+              monthly ? "Monthly" : "Yearly"
+            })`}</h2>
+            <button className="text-sm underline text-neutral-500">
+              Change
+            </button>
           </div>
-          <div className="font-bold">234</div>
+          <div className="font-medium">{`$${
+            monthly ? selectedPlan.monthly : selectedPlan.yearly
+          }/${monthly ? "mo" : "yr"}`}</div>
         </div>
-        <hr className="bg-neutral-400 border-none h-[2px]"/>
-        <div>add ons...</div>
+        {addOns.length > 0 && (
+          <hr className="bg-neutral-400 border-none h-[2px]" />
+        )}
+        {addOns.map((addOn) => (
+          <AddOnSummary key={addOn.title} addOn={addOn} />
+        ))}
+        <hr className="bg-neutral-400 border-none h-[2px]" />
+        {addOns.map((addOn) => (
+          <AddOnSummary key={addOn.title} addOn={addOn} />
+        ))}
       </div>
       <div className="flex justify-between items-center p-5">
-        <span className="text-neutral-500 text-sm">Total:</span>
-        <span className="text-blue-300 font-bold">2435</span>
+        <span className="text-neutral-500 text-sm">{`Total ${
+          monthly ? "(per month)" : "(per year)"
+        }`}</span>
+        <span className="text-blue-300 font-bold">{`$${total(
+          selectedPlan,
+          addOns,
+          monthly
+        )}/${monthly ? "mo" : "yr"}`}</span>
       </div>
     </div>
   );
