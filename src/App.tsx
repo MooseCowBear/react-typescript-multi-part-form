1645293997b34aa62ba3ca873a12557e13490bd4
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-01-11 08:23:38 -0500

    rework form component for form buttons always appear within it and have access to form context

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import { Sidebar } from "./layout/Sidebar";
 import { Form } from "./layout/Form";
 import { StepProvider } from "./contexts/StepContext";
-import { FormButtonsWrapper } from "./components/FormButtonsWrapper";
-
-// form buttons need access to form context, so this has to change
 
 function App() {
   return (
     <StepProvider>
       <Sidebar />
-      <div className="bg-neutral-300 md:bg-neutral-100 relative md:h-full p-5 md:p-0 min-h-[520px]">
-        <Form />
-        <FormButtonsWrapper classes="absolute p-3 bg-neutral-100 bottom-0 left-0 block w-full md:hidden" />
-      </div>
+      <Form />
     </StepProvider>
   );
 }
